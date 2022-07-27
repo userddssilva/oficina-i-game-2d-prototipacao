d448d5588678da6fb9aa405e44a51fdc8abf6dbe
Dayvson Silva <dayvson.silva@samsung.com>
2022-07-27 11:52:51 -0400

    Add collected items to the game

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private CircleCollider2D circle;
+
+    public GameObject collected;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            spriteRenderer.enabled = false;
+            circle.enabled = false;
+
+            collected.SetActive(true);
+
+
+            Destroy(gameObject, 1f);
+        }
+    }
+}
